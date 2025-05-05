51ff5cfc14e5cd0d02d8038ed32d4cbca18ca5f4
Asmit Katiyar <asmitkatiyar33@gmail.com>
2025-05-06 00:13:04 +0530

    deployed link in about section added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,21 @@
 const About = () => {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>About Loan Calculator</h1>
-      <p>
+      <h1 style={{ margin: "1rem" }}>About Loan Calculator</h1>
+      <p style={{ margin: "1rem" }}>
         This tool helps you calculate EMIs and view amortization schedules. It
         also supports live currency conversions and provides up-to-date exchange
         rates.
       </p>
+
+      <h2 style={{ margin: "1rem" }}>Get the deployed project here</h2>
+      <a
+        style={{ margin: "1rem" }}
+        href="https://sunny-stardust-b5c427.netlify.app/"
+        target="_blank"
+      >
+        React Project
+      </a>
     </div>
   );
 };
